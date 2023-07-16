de1520eb7946a3cb370de8616e657c80c5100a77
olivertembo <tembokoliver3@gmail.com>
2023-07-16 17:38:08 -0400

    show selected item on the left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,81 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { Box, Typography, Drawer, List, ListItem, ListItemText, ListItemButton, Collapse } from '@mui/material';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+type NavigationItem = {
+  title: string;
+  children?: NavigationItem[];
+};
+
+const navigationItems: NavigationItem[] = [
+  { title: 'Item 1', children: [{ title: 'Subitem 1' }, { title: 'Subitem 2' }] },
+  { title: 'Item 2' },
+  { title: 'Item 3' },
+  { title: 'Item 4' },
+];
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [selectedItem, setSelectedItem] = useState('');
+  const [openItems, setOpenItems] = useState<string[]>([]);
+
+  const handleToggle = (item: string) => {
+    const currentIndex = openItems.indexOf(item);
+    const newOpenItems = [...openItems];
+
+    if (currentIndex === -1) {
+      newOpenItems.push(item);
+    } else {
+      newOpenItems.splice(currentIndex, 1);
+    }
+
+    setSelectedItem(item);
+
+    setOpenItems(newOpenItems);
+  };
+
+  const renderNavigationItems = (items: NavigationItem[], depth = 0) => (
+    <List>
+      {items.map((item) => (
+        <div key={item.title}>
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => handleToggle(item.title)}>
+              <ListItemText primary={item.title} style={{ paddingLeft: depth * 20 }} />
+              {openItems.includes(item.title) ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </ListItem>
+          <Collapse in={openItems.includes(item.title)} timeout="auto" unmountOnExit>
+            {item.children && renderNavigationItems(item.children, depth + 1)}
+          </Collapse>
         </div>
-      </div>
+      ))}
+    </List>
+  );
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+  console.log("selectedItem", selectedItem);
+  return (
+    <main>
+      <Box sx={{ display: "flex" }}>
+        <Drawer variant="permanent" open>
+          {renderNavigationItems(navigationItems)}
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            p: 3,
+            width: "100%",
+            marginLeft: "184px",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <Typography>{selectedItem}</Typography>
+        </Box>
+      </Box>
     </main>
-  )
+  );
 }
