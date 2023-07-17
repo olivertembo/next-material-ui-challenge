f30ce86da5d3a1edc1cebb30f31e2eb613757337
olivertembo <tembokoliver3@gmail.com>
2023-07-17 09:05:38 -0400

    add unique keys to open items state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,103 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Typography, Drawer, List, ListItem, ListItemText, ListItemButton, Collapse } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import * as styles from './styles';
 
-type NavigationItemBase = {
+type NavigationItem = {
   title: string;
-};
-
-type NavigationItemWithRoute = NavigationItemBase & {
   route?: string;
+  children?: NavigationItem[];
 };
-
-type NavigationItemWithChildren = NavigationItemBase & {
-  route?: never;
-  children: NavigationItem[];
-};
-
-type NavigationItem = NavigationItemWithRoute | NavigationItemWithChildren;
 
 const navigationItems: NavigationItem[] = [
-  { title: 'Item 1', children: [{ title: 'Subitem 1',  route: '/about' }, { title: 'Subitem 2' }] },
-  { title: 'Item 2', children: [{ title: 'Subitem 21' }, { title: 'Subitem 22' }] },
-  { title: 'Item 3', route: '/item-3', },
-  { title: 'Item 4', route: '/item-4', },
+  {
+    title: "Item 1",
+    children: [
+      {
+        title: "Subitem 1",
+        route: "/about",
+        children: [
+          { title: "Subitem 1", route: "/about" },
+          {
+            title: "Subitem 2",
+            children: [
+              {
+                title: "Subitem 1",
+                route: "/about",
+                children: [
+                  { title: "Subitem 1", route: "/about" },
+                  {
+                    title: "Subitem 2",
+                    children: [
+                      {
+                        title: "Subitem 1",
+                        route: "/about",
+                        children: [
+                          { title: "Subitem 1", route: "/about" },
+                          { title: "Subitem 2" },
+                        ],
+                      },
+                      { title: "Subitem 2" },
+                    ],
+                  },
+                ],
+              },
+              { title: "Subitem 2" },
+            ],
+          },
+        ],
+      },
+      { title: "Subitem 2" },
+    ],
+  },
+  {
+    title: "Item 2",
+    route: "/item-2",
+    children: [{ title: "Subitem 21" }, { title: "Subitem 22" }],
+  },
+  { title: "Item 3", route: "/item-3" },
+  { title: "Item 4", route: "/item-4" },
 ];
 
 export default function Home() {
-  const [selectedItem, setSelectedItem] = useState('');
-  const [openItems, setOpenItems] = useState<string[]>([]);
+  const [selectedItem, setSelectedItem] = useState(localStorage.getItem("selectedItem") || '');
+  const [openItems, setOpenItems] = useState<string[]>(localStorage.getItem("openItems")?.split(',') || []);
 
-  const handleToggle = (item: NavigationItem) => {
-    const currentIndex = openItems.indexOf(item.title);
+  const handleToggle = (key: string) => {
+    const currentIndex = openItems.indexOf(key);
     const newOpenItems = [...openItems];
 
     if (currentIndex === -1) {
-      newOpenItems.push(item.title);
+      newOpenItems.push(key);
     } else {
       newOpenItems.splice(currentIndex, 1);
     }
-
-    if (!item?.children) {
-      setSelectedItem(item.title);
-    }
-
+    setSelectedItem(key);
     setOpenItems(newOpenItems);
   };
 
+  useEffect(() => {
+    localStorage.setItem("selectedItem", selectedItem);
+    localStorage.setItem("openItems", openItems.join(','));
+  }, [selectedItem, openItems]);
+
   const renderNavigationItems = (items: NavigationItem[], depth = 0) => (
     <List>
-      {items.map((item) => (
-        <div key={item.title}>
+      {items.map((item, index) => (
+        <div key={`${item.title}-${index}-${depth}`}>
           <ListItem disablePadding>
             <ListItemButton
               selected={item.title === selectedItem}
-              onClick={() => handleToggle(item)}
+              onClick={() => handleToggle(`${item.title}-${index}-${depth}`)}
             >
               <ListItemText primary={item.title} style={{ paddingLeft: depth * 20 }} />
-              {item.children && (openItems.includes(item.title) ? <ExpandLess /> : <ExpandMore />)}
+              {item.children ? (openItems.includes(`${item.title}-${index}-${depth}`) ? <ExpandLess /> : <ExpandMore />) : null}
             </ListItemButton>
           </ListItem>
-          <Collapse in={openItems.includes(item.title)} timeout="auto" unmountOnExit>
+          <Collapse in={openItems.includes(`${item.title}-${index}-${depth}`)} timeout="auto" unmountOnExit>
             {item.children && renderNavigationItems(item.children, depth + 1)}
           </Collapse>
         </div>
